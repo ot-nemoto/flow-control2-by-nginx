61c4022de148e4551945114c14c37dafde6dadb6
ot-nemoto <nemoto@opentone.co.jp>
2021-08-16 02:58:52 +0000

    add post method pattern

--- README.md
+++ README.md
@@ -4,11 +4,15 @@
 
 - nginxに流量制限を設定し検証
 
-|Path|流量制限|HTTPステータス|
-|--|--|--|
-|/|-|-|
-|/1.html|10req/s|429|
-|/2.html|30req/s|503|
+|Path|Method|流量制限|HTTPステータス|
+|--|--|--|--|
+|/|GET-|-|
+|/1.html|GET|10req/s(all)|429|
+|/2.html|GET|30req/s(all)|503|
+|/3.html|POST|-|-|
+|/4.html|POST|10req/s(all)|429|
+|/5.html|POST|10req/s($uri)|429|
+|/6.html|POST|30req/s($uri)|429|
 
 - appはGradleのデモアプリ
 
--- app/src/main/java/jp/co/opentone/app/AppApplication.java
+++ app/src/main/java/jp/co/opentone/app/AppApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @SpringBootApplication
 @RestController
@@ -15,13 +16,33 @@ public class AppApplication {
 	}
 
 	@RequestMapping("/1.html")
-	public String home1() {
+	public String html1() {
 		return "1";
 	}
 
 	@RequestMapping("/2.html")
-	public String home2() {
+	public String html2() {
 		return "2";
+	}
+
+	@RequestMapping(path = "/3.html", method = POST)
+	public String html3() {
+		return "3";
+	}
+
+	@RequestMapping(path = "/4.html", method = POST)
+	public String html4() {
+		return "4";
+	}
+
+	@RequestMapping(path = "/5.html", method = POST)
+	public String html5() {
+		return "5";
+	}
+
+	@RequestMapping(path = "/6.html", method = POST)
+	public String html6() {
+		return "6";
 	}
 
 	public static void main(String[] args) {
